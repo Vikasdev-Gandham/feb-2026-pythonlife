f81ae4f19fa3f6b9e7d0845d6450e9dc3d367ff9
Vikasdev Gandham <88623327+Vikasdev-Gandham@users.noreply.github.com>
2026-05-17 16:38:22 +0530

    local storage
    python pratice new files

---
+++ loops1.py
@@ -0,0 +1,80 @@
+#for variable in sequence:
+ # code to be executed
+
+# voter_data = ["prashant", "prakash", "raju", "sangeetha", "divya"]
+# for item in voter_data:
+#     print("welcome to pythonlife")
+
+# voter_data = ["prashant", "prakash", "raju", "sangeetha", "divya"]
+# for item in voter_data:
+#     print(f"welcome to pythonlife {item}")
+
+# fruits = {"orange", "grapes", "bananas"}
+# for item in fruits:
+#     if item == "bananas":
+#         print(f"{item} has been received")
+
+
+#syntax
+#range(stop)
+#range(start,stop)
+#range(start,stop,step)
+
+
+# for i in range(11):
+#     print(f"{i}")
+
+#stop value always considered (n-1)
+# for i in range(1,999999999999):
+#     print(f"{i}")
+
+
+# for i in range(1,11,1):
+#     print(f"{i}")
+
+# for i in range(1,11,2):
+#     print(f"{i}")
+
+# for i in range(1,11,3):
+#     print(f"{i}")
+
+
+#prinnt odd numbers from 0 to 100
+# for i in range(0,100,2):
+#     print(f"{i}")
+
+ #print even numbers from 499 to 5000
+# for i in range(499,5000):
+#     if i%2==0:
+#         print(i)
+
+# mulitplication table
+# for i in range(1,21):
+#     print(f"2 x {i} = {i*2}") 
+
+# for i in range(1,11):
+#     print(f"17 x {i} = {i*17}")
+
+# user input number multiplicatoin table 
+# table = int(input("enther the table: "))
+# for i in range(1,11):
+#     print(f"{table} x {i} = {table*i}")
+
+#nested for loop
+# for i in seq:
+#       for i in seq
+
+# for i in range(4): #outer for loop
+#     for j in range(4): # inner for loop
+#         print(i,j)
+
+# multiplication tables 1 to 10th table
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{i} x {j} = {i*j}")
+
+# multiplication table 10 to 20 table
+# for i in range (10,21):
+#     for j in range (1,21):
+#         print(f"{i} x {j} = {i*j}")
+#     print("_"*15)
